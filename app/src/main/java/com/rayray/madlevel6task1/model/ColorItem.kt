d99c9dabcc49374f470ddc552bdaa670ae7d53ac
raymondbdws <raymondbdws@hotmail.com>
2020-11-08 16:18:31 +0100

    init

---
+++ app/src/main/java/com/rayray/madlevel6task1/model/ColorItem.kt
@@ -0,0 +1,8 @@
+package com.rayray.madlevel6task1.model
+
+data class ColorItem (
+    var hex: String,
+    var name: String
+){
+    fun getImageUrl() = "http://singlecolorimage.com/get/$hex/1080x1080"
+}
